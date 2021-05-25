f2898a64ea33006c3636b8dd9698e1c18168eed1
IamTamika <taylorlewis000@gmail.com>
2021-05-24 19:35:49 -0600

    Added Registration model

---
+++ SportsPro.Domain/Models/Registration.cs
@@ -0,0 +1,19 @@
+﻿using SportsPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsPro.Domain.Models
+{
+    public class Registration
+    {
+        public int CustomerID { get; set; }
+        public int ProductID { get; set; }
+
+
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+    }
+}
