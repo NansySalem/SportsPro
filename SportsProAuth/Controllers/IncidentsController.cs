008c2f5d6f9cd420c447bfaa6f87ba619c0bf113
nansysalem <nancy.moustafa@gmail.com>
2021-05-24 20:17:33 -0600

    Updated all crud views titles updated incident controller

--- SportsProAuth/Controllers/IncidentsController.cs
+++ SportsProAuth/Controllers/IncidentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using SportsPro.Models;
 
 namespace SportsProAuth.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class IncidentsController : Controller
     {
         private readonly SportsProContext _context;
@@ -19,6 +21,8 @@ namespace SportsProAuth.Controllers
         }
 
         // GET: Incidents
+        
+        //[Authorize(Roles = "Technician")]
         public async Task<IActionResult> Index()
         {
             var sportsProContext = _context.Incidents.Include(i => i.Customer).Include(i => i.Product).Include(i => i.Technician);
@@ -26,6 +30,7 @@ namespace SportsProAuth.Controllers
         }
 
         // GET: Incidents/Details/5
+      //  [Authorize(Roles = "Technician")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -50,7 +55,105 @@ namespace SportsProAuth.Controllers
         {
             return _context.Incidents.Any(e => e.IncidentID == id);
         }
+
+
+        // GET: Incidents/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerID", "FullName", null);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ProductID", "Name", null);
+            ViewData["TechnicianID"] = new SelectList(_context.Technicians, "TechnicianID", "Name", null);
+            return View();
+        }
+
+        // POST: Incidents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IncidentID,CustomerID,ProductID,TechnicianID,Title,Description,DateOpened,DateClosed")] Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(incident);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerID", "FullName", incident.CustomerID);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ProductID", "Name", incident.ProductID);
+            ViewData["TechnicianID"] = new SelectList(_context.Technicians, "TechnicianID", "TechnicianID", incident.TechnicianID);
+            return View(incident);
+        }
+
+        // GET: Incidents/Edit/5
+       // [Authorize(Roles = "Technician")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var incident = await _context.Incidents.FindAsync(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerID", "FullName", incident.CustomerID);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ProductID", "Name", incident.ProductID);
+            if (incident.TechnicianID != null)
+                ViewData["TechnicianID"] = new SelectList(_context.Technicians, "TechnicianID", "TechnicianID", incident.TechnicianID);
+            else
+                ViewData["TechnicianID"] = new SelectList(_context.Technicians, "TechnicianID", "TechnicianID", null);
+            return View(incident);
+        }
+
+        // POST: Incidents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+      //  [Authorize(Roles = "Technician")]
+        public async Task<IActionResult> Edit(int id, [Bind("IncidentID,CustomerID,ProductID,TechnicianID,Title,Description,DateOpened,DateClosed")] Incident incident)
+        {
+            if (id != incident.IncidentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(incident);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IncidentExists(incident.IncidentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerID", "Address", incident.CustomerID);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ProductID", "Name", incident.ProductID);
+            ViewData["TechnicianID"] = new SelectList(_context.Technicians, "TechnicianID", "Email", incident.TechnicianID);
+            return View(incident);
+        }
         //edit and create in one view
+        /// <summary>
+        /// As discussed, created a sample for the logic of having create and edit using one view "upsert.cshtml"
+        ///Although the logic works, had to use different views for the edit and create 
+        ///to allow different authorization priviledges i.e. create for admin and edit/update for technician
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> Upsert(int? id)
         {
             if (id == null)//if id is null, then the request is to create a new incident
